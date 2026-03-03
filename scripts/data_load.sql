0a6373bd45981f927cacced1a011d88f8a463b4f
Danaya DIARRA <127289627+DanayaDiarra@users.noreply.github.com>
2026-03-03 23:45:56 +0300

    Refactor bronze.load_bronze procedure
    Refactored bronze.load_bronze procedure to improve data loading process and error handling. Removed redundant BULK INSERT statements and added structured error handling.

--- scripts/data_load.sql
+++ scripts/data_load.sql
@@ -1,112 +1,239 @@
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+CREATE OR ALTER PROCEDURE bronze.load_bronze
+AS
 BEGIN
-	/*
-	====================================================================
-	SQL FULL DATA LOAD _ bronze_layer
-	This script uses BULK INSERT  to load the entire data(file) to the db.
-	This script is saved as a 'PROCEDURE'in Folder 'Programmability'. 
-	
-	To automatically run it, use: "EXECUTE bronze.load_bronze"
-	====================================================================
-	*/
-	PRINT '==============================================================';
-	PRINT 'LOADING bronze layer';
-	PRINT '==============================================================';
-	
-	PRINT'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++';
-	PRINT '>>Loading CRM Tables';
-	PRINT'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++';
+    --SET NOCOUNT ON;  -- Prevents 'rows affected' messages for cleaner output
+    --SET XACT_ABORT ON; -- Auto-rollback on error
+    
+    BEGIN TRY
+        /*
+        ====================================================================
+        SQL FULL DATA LOAD - BRONZE LAYER
+        This procedure(in Folder 'Programmability') loads all source data files into bronze layer tables
+        using BULK INSERT operations.
+        
+        Author: DIARRA Danaya
+        Created: 03/03/2026
+        Last Modified: ../../..
+        
+        Execution: EXEC bronze.load_bronze
+        ====================================================================
+        */
+        
+        -- ====================================================================
+        -- START OF LOADING PROCESS
+        -- ====================================================================
+        PRINT '==============================================================';
+        PRINT 'STARTING BRONZE LAYER LOAD PROCESS';
+        PRINT '==============================================================';
+        PRINT 'Start Time: ' + CONVERT(VARCHAR(30), GETDATE(), 120);
+        PRINT '';
+        
+        -- ====================================================================
+        -- CRM TABLES LOADING
+        -- ====================================================================
+        PRINT '+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +';
+        PRINT 'Loading CRM Source Tables';
+        PRINT '+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +';
+        PRINT '';
 
-	PRINT'---------------------------------------------------------------';
-	PRINT'Truncated and Loaded data to table:crm_cust_info ';
-	PRINT'---------------------------------------------------------------';
+        -- --------------------------------------------------------------------
+        -- Table: bronze.crm_cust_info
+        -- Source: source_crm\cust_info.csv
+        -- --------------------------------------------------------------------
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Table: crm_cust_info';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Truncating table...';
+        
+        TRUNCATE TABLE bronze.crm_cust_info;
+        
+        PRINT 'Loading data from CSV...';
+        BULK INSERT bronze.crm_cust_info
+        FROM 'C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_crm\cust_info.csv'
+        WITH (
+            FIRSTROW = 2,           -- Skip header row
+            FIELDTERMINATOR = ',',   -- Comma delimited
+            ROWTERMINATOR = '\n',    -- Row terminator
+            TABLOCK,                 -- Table locking for performance
+            CODEPAGE = '65001'       -- UTF-8 encoding
+        );
+        
+        PRINT 'Table crm_cust_info loaded successfully';
+        PRINT 'Rows loaded: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+        PRINT '';
 
-	TRUNCATE TABLE bronze.crm_cust_info;
-	BULK INSERT bronze.crm_cust_info
-	FROM "C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_crm\cust_info.csv"
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '\n', 
-		TABLOCK,
-		CODEPAGE = '65001' 
-	);
-	PRINT'---------------------------------------------------------------';
-	PRINT'Truncated and Loaded data to table:crm_prd_info ';
-	PRINT'---------------------------------------------------------------';
+        -- --------------------------------------------------------------------
+        -- Table: bronze.crm_prd_info
+        -- Source: source_crm\prd_info.csv
+        -- --------------------------------------------------------------------
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Table: crm_prd_info';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Truncating table...';
+        
+        TRUNCATE TABLE bronze.crm_prd_info;
+        
+        PRINT 'Loading data from CSV...';
+        BULK INSERT bronze.crm_prd_info
+        FROM 'C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_crm\prd_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK,
+            CODEPAGE = '65001'
+        );
+        
+        PRINT 'Table crm_prd_info loaded successfully';
+        PRINT 'Rows loaded: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+        PRINT '';
 
-	TRUNCATE TABLE bronze.crm_prd_info;
-	BULK INSERT bronze.crm_prd_info
-	FROM "C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_crm\prd_info.csv"
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '\n', 
-		TABLOCK,
-		CODEPAGE = '65001' 
-	);
-	PRINT'---------------------------------------------------------------';
-	PRINT'Truncated and Loaded data to table:crm_sales_details ';
-	PRINT'---------------------------------------------------------------';
+        -- --------------------------------------------------------------------
+        -- Table: bronze.crm_sales_details
+        -- Source: source_crm\sales_details.csv
+        -- --------------------------------------------------------------------
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Table: crm_sales_details';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Truncating table...';
+        
+        TRUNCATE TABLE bronze.crm_sales_details;
+        
+        PRINT 'Loading data from CSV...';
+        BULK INSERT bronze.crm_sales_details
+        FROM 'C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_crm\sales_details.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK,
+            CODEPAGE = '65001'
+        );
+        
+        PRINT 'Table crm_sales_details loaded successfully';
+        PRINT 'Rows loaded: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+        PRINT '';
 
-	TRUNCATE TABLE bronze.crm_sales_details;
-	BULK INSERT bronze.crm_sales_details
-	FROM "C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_crm\sales_details.csv"
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '\n', 
-		TABLOCK,
-		CODEPAGE = '65001' 
-	);
+        -- ====================================================================
+        -- ERP TABLES LOADING
+        -- ====================================================================
+        PRINT '+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +';
+        PRINT 'Loading ERP Source Tables';
+        PRINT '+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +';
+        PRINT '';
 
-	PRINT'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++';
-	PRINT '>>Loading ERP Tables';
-	PRINT'+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++';
-	
-	PRINT'---------------------------------------------------------------';
-	PRINT'Truncated and Loaded data to table:erp_cust_az12 ';
-	PRINT'---------------------------------------------------------------';
+        -- --------------------------------------------------------------------
+        -- Table: bronze.erp_cust_az12
+        -- Source: source_erp\CUST_AZ12.csv
+        -- --------------------------------------------------------------------
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Table: erp_cust_az12';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Truncating table...';
+        
+        TRUNCATE TABLE bronze.erp_cust_az12;
+        
+        PRINT 'Loading data from CSV...';
+        BULK INSERT bronze.erp_cust_az12
+        FROM 'C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_erp\CUST_AZ12.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK,
+            CODEPAGE = '65001'
+        );
+        
+        PRINT 'Table erp_cust_az12 loaded successfully';
+        PRINT 'Rows loaded: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+        PRINT '';
 
-	TRUNCATE TABLE bronze.erp_cust_az12;
-	BULK INSERT bronze.erp_cust_az12
-	FROM "C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_erp\CUST_AZ12.csv"
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '\n', 
-		TABLOCK,
-		CODEPAGE = '65001' 
-	);
+        -- --------------------------------------------------------------------
+        -- Table: bronze.erp_loc_a101
+        -- Source: source_erp\LOC_A101.csv
+        -- --------------------------------------------------------------------
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Table: erp_loc_a101';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Truncating table...';
+        
+        TRUNCATE TABLE bronze.erp_loc_a101;
+        
+        PRINT 'Loading data from CSV...';
+        BULK INSERT bronze.erp_loc_a101
+        FROM 'C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_erp\LOC_A101.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK,
+            CODEPAGE = '65001'
+        );
+        
+        PRINT 'Table erp_loc_a101 loaded successfully';
+        PRINT 'Rows loaded: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+        PRINT '';
 
-	PRINT'---------------------------------------------------------------';
-	PRINT'Truncated and Loaded data to table:erp_loc_a101 ';
-	PRINT'---------------------------------------------------------------';
+        -- --------------------------------------------------------------------
+        -- Table: bronze.erp_px_cat_g1v2
+        -- Source: source_erp\PX_CAT_G1V2.csv
+        -- --------------------------------------------------------------------
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Table: erp_px_cat_g1v2';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Truncating table...';
+        
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+        
+        PRINT 'Loading data from CSV...';
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM 'C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_erp\PX_CAT_G1V2.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '\n',
+            TABLOCK,
+            CODEPAGE = '65001'
+        );
+        
+        PRINT ' Table erp_px_cat_g1v2 loaded successfully';
+        PRINT ' Rows loaded: ' + CAST(@@ROWCOUNT AS VARCHAR(10));
+        PRINT '';
 
-	TRUNCATE TABLE bronze.erp_loc_a101;
-	BULK INSERT bronze.erp_loc_a101
-	FROM "C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_erp\LOC_A101.csv"
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '\n', 
-		TABLOCK,
-		CODEPAGE = '65001' 
-	);
+        -- ====================================================================
+        -- LOADING COMPLETED SUCCESSFULLY
+        -- ====================================================================
+        PRINT '==============================================================';
+        PRINT ' BRONZE LAYER LOAD COMPLETED SUCCESSFULLY';
+        PRINT '==============================================================';
+        PRINT 'End Time: ' + CONVERT(VARCHAR(30), GETDATE(), 120);
+        PRINT '';
 
-	PRINT'---------------------------------------------------------------';
-	PRINT'Truncated and Loaded data to table:erp_px_cat_g1v2 ';
-	PRINT'---------------------------------------------------------------';
-
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM "C:\Applied Project 2026\Data_Warehouse_and_BI\datasets\source_erp\PX_CAT_G1V2.csv"
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		ROWTERMINATOR = '\n', 
-		TABLOCK,
-		CODEPAGE = '65001' 
-	);
-END
-
+    END TRY
+    BEGIN CATCH
+        -- ====================================================================
+        -- ERROR HANDLING
+        -- ====================================================================
+        PRINT '==============================================================';
+        PRINT ' ERROR OCCURRED DURING BRONZE LAYER LOAD';
+        PRINT '==============================================================';
+        PRINT 'Error Time: ' + CONVERT(VARCHAR(30), GETDATE(), 120);
+        PRINT '';
+        PRINT 'Error Details:';
+        PRINT '---------------------------------------------------------------';
+        PRINT 'Error Number   : ' + CAST(ERROR_NUMBER() AS VARCHAR(10));
+        PRINT 'Error Severity : ' + CAST(ERROR_SEVERITY() AS VARCHAR(10));
+        PRINT 'Error State    : ' + CAST(ERROR_STATE() AS VARCHAR(10));
+        PRINT 'Error Line     : ' + CAST(ERROR_LINE() AS VARCHAR(10));
+        PRINT 'Error Procedure: ' + ISNULL(ERROR_PROCEDURE(), 'N/A');
+        PRINT '';
+        PRINT 'Error Message:';
+        PRINT '---------------------------------------------------------------';
+        PRINT ERROR_MESSAGE();
+        PRINT '==============================================================';
+        
+        -- Re-throw the error for application handling
+        THROW;
+    END CATCH
+END;
+GO
